903025061d1054e878ec44c7638db26e7cd2c880
PottierLoic <loic.pottier@live.fr>
2023-01-31 23:36:03 +0100

    removed useless thing

--- main.py
+++ main.py
@@ -605,16 +605,6 @@ if __name__=="__main__":
 
     window.update()
 
-    windowWidth = window.winfo_width()
-    windowHeight = window.winfo_height()
-    screenWidth = window.winfo_screenwidth()
-    screenHeight = window.winfo_screenheight()
-
-    x = int((screenWidth/2) - (windowWidth/2))
-    y = int((screenHeight/2) - (windowHeight/2))
-
-    window.geometry(f"{windowWidth}x{windowHeight}+{x}+{y}")
-
     b = board.Board()
 
     # KEYBINDS (if you want to play without ai)
